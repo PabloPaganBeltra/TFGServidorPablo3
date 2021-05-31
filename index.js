6146825348027c9eee6387041ad22145d0db27e4
PabloPaganBeltra <56672765+PabloPaganBeltra@users.noreply.github.com>
2021-05-31 20:50:41 +0200

    *

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 conectarDB();
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "domain"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
     next();

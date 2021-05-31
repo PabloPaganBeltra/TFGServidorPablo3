39531fd8929c78ac73215e5067e7dbfec78bbab8
PabloPaganBeltra <56672765+PabloPaganBeltra@users.noreply.github.com>
2021-05-31 18:51:20 +0200

    SolucionCors3

--- index.js
+++ index.js
@@ -29,6 +29,14 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 //Arrancar server
 app.listen(port, '0.0.0.0', ()=>{
     console.log('El servidor esta funcionando en el puerto',port);

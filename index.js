eb8cfccb638ae3235df2b95565f33ba1ba1668bc
PabloPaganBeltra <56672765+PabloPaganBeltra@users.noreply.github.com>
2021-05-31 19:59:23 +0200

    Cors

--- index.js
+++ index.js
@@ -9,11 +9,10 @@ const app = express();
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
-app.options('*', cors());
 
 //Conectar a la base de datos
 conectarDB();

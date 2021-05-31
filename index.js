e28f6b682a7d7cac93010cd29095118768d84853
PabloPaganBeltra <56672765+PabloPaganBeltra@users.noreply.github.com>
2021-05-31 20:49:19 +0200

    dfsd

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const app = express();
 //Conectar a la base de datos
 conectarDB();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "domain"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    next();
+  });
+
 //habilitar cors
 app.use(cors({ credentials: true, origin: true }));
 app.options("*", cors());

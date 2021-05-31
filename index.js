dc8095d5946d541aaf90a668c7aa2ff6785e387b
PabloPaganBeltra <56672765+PabloPaganBeltra@users.noreply.github.com>
2021-05-31 23:14:14 +0200

    asdf

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 //  Habla algo de "erocu", se supone que revisa si tenemos un puerto configurado, si no, utilizará el 4000
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 //importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -24,6 +24,6 @@ app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
 //  Arrancamos el servidor
-app.listen(PORT, () => {
-  console.log(`El servidor está funcionando en el puerto ${PORT}`);
+app.listen(port, () => {
+  console.log(`El servidor está funcionando en el puerto ${port}`);
 });

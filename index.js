30d19b75611bb9a9bf21f301278cf770d77ff28a
PabloPaganBeltra <56672765+PabloPaganBeltra@users.noreply.github.com>
2021-05-31 21:05:00 +0200

    dsf

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const app = express();
 conectarDB();
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "*"); 
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
     next();
   });
 
 //habilitar cors
-app.use(cors({ credentials: true, origin: true }));
+app.use(cors({ credentials: true }));
 app.options("*", cors());
 
 //habilitar express.json
